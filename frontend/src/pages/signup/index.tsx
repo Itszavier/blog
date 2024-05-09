7cf2f0caad1dabb3fac3e04c47e1481c0ed80d47
wolfking <imanibrown421@gmail.com>
2024-05-09 00:54:55 +0000

    color veriants

--- frontend/src/pages/signup/index.tsx
+++ frontend/src/pages/signup/index.tsx
@@ -1,5 +1,34 @@
 import style from "./style.module.css";
 
 export default function Signup() {
-  return <div className={style.container}>signup page</div>;
+  return (
+    <div className={style.container}>
+      <form className={style.form}>
+        <div className={style.header}>create a new account</div>
+        <div className={style.input_group}>
+          <div className={style.input_wrapper}>
+            <span className={`material-icons  ${style.icon}`}>mail</span>
+            <input
+              className={style.input}
+              type="email"
+              required
+              placeholder="Email"
+            />
+          </div>
+
+          <div className={style.input_wrapper}>
+            <span className={`material-icons ${style.icon}`}>lock</span>
+            <input
+              className={style.input}
+              type="email"
+              required
+              placeholder="Password"
+            />
+          </div>
+        </div>
+        <div className={style.error_message}>error message</div>
+        <button className={style.signup_btn}>Sign up</button>
+      </form>
+    </div>
+  );
 }
