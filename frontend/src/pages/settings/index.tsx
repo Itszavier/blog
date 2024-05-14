5986549707adfa4bb48c57b00b96223e20855711
wolfking <imanibrown421@gmail.com>
2024-05-14 12:59:58 +0000

    settings page

--- frontend/src/pages/settings/index.tsx
+++ frontend/src/pages/settings/index.tsx
@@ -5,9 +5,27 @@ import style from "./style.module.css";
 export default function Settings() {
   return (
     <div className={style.container}>
-      <div className={style.settings_tab}>tab</div>
+      <div className={style.settings_tab}>tabs</div>
       <form className={style.profile_settings_form}>
-        <div></div>
+        <div className={style.input_group}>
+          <div className={style.input_wrapper}>
+            <span className={style.label_text}>full name</span>
+            <input
+              className={style.input}
+              type="text"
+              defaultValue={"Ash Lord"}
+            />
+          </div>
+
+          <div className={style.input_wrapper}>
+            <span className={style.label_text}>Bio</span>
+            <input
+              className={style.input}
+              type="text"
+              defaultValue={"I am a avid writer"}
+            />
+          </div>
+        </div>
       </form>
     </div>
   );
