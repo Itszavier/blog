df3cb6f748e2e4efaf494ffd63f0e47570693263
wolfking <imanibrown421@gmail.com>
2024-05-14 12:45:45 +0000

    updated

--- frontend/src/pages/settings/index.tsx
+++ frontend/src/pages/settings/index.tsx
@@ -3,5 +3,12 @@
 import style from "./style.module.css";
 
 export default function Settings() {
-  return <div className={style.container}>settings page</div>;
+  return (
+    <div className={style.container}>
+      <div className={style.settings_tab}>tab</div>
+      <form className={style.profile_settings_form}>
+        <div></div>
+      </form>
+    </div>
+  );
 }
