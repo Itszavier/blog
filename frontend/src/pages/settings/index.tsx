1e74f0f76cd1b38b54c39937aa63616556d40c68
silentwolf-dev <omariimanibrown421@gmail.com>
2024-05-23 15:57:47 -0400

    small changes

--- frontend/src/pages/settings/index.tsx
+++ frontend/src/pages/settings/index.tsx
@@ -3,5 +3,11 @@
 import style from "./style.module.css";
 
 export default function Settings() {
-  return <div className={style.container}>settings page</div>;
+  return (
+    <div className={style.container}>
+      <div className={style.nav_header}>
+        hello world
+      </div>
+    </div>
+  );
 }
