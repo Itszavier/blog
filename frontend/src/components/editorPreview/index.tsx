f9e60be9a7d8fe437c95d46bc1f1441d1c883625
silentwolf-dev <omariimanibrown421@gmail.com>
2024-07-30 10:16:58 -0400

    purged files

--- frontend/src/components/editorPreview/index.tsx
+++
@@ -1,6 +0,0 @@
-/** @format */
-import style from "./style.module.css";
-
-export default function EditorPreview({ content }: { content: string }) {
-  return <div className={style.preview} dangerouslySetInnerHTML={{ __html: content }} />;
-}
